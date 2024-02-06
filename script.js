8e5ba6e4b5de8d65a9f61936be04e712d74b3d62
Gabriel Samuel <samuelgab246@gmail.com>
2024-02-06 10:54:05 -0800

    advanced JS

---
+++ script.js
@@ -0,0 +1,12 @@
+const obj = {
+    player:'bobby',
+    experience: 100,
+    WizardLevel: false
+}
+
+const player = obj.player;
+const experience = obj.experience;
+let WizardLevel = obj.WizardLevel;
+
+// const { player, experience} = obj;
+// let { WizardLevel } = obj;
